398513dc70565c024f959801deb6d64b35cb917f
Khristina <christina.marynovych@gmail.com>
2020-11-12 12:39:51 +0200

    updates in code add customersMoney and cut discount\

--- script.js
+++ script.js
@@ -5,11 +5,12 @@ const sumOfGoods = scarf + shoes + coat;
 const aproxSumOfGoods = Math.floor(scarf) + Math.floor(shoes) + Math.floor(coat);
 const hundredsSum = Math.floor(aproxSumOfGoods/100)*100;
 const isEven = sumOfGoods % 2 == 0;
-const changeFromPayment = 500 - sumOfGoods;
+const customersMoney = 500;
+const changeFromPayment = customersMoney - sumOfGoods;
 const averageAprox = (sumOfGoods / 3).toFixed(2);
-let randomDiscount = Math.random()*100;
-let sumWithDiscount = ((1 - randomDiscount/100) * sumOfGoods).toFixed(2);
-let netProfit = sumOfGoods/2 - sumOfGoods*randomDiscount/100;
+const randomDiscount = Math.random()*50;
+const sumWithDiscount = ((1 - randomDiscount/100) * sumOfGoods).toFixed(2);
+const netProfit = sumOfGoods/2 - sumOfGoods*randomDiscount/100;
  console.log('the highest prise in shop is ' + Math.max(scarf, shoes, coat));
  console.log('the lowest prise in shop is ' + Math.min(scarf, shoes, coat));
  console.log('the total sum of money spent on goods is ' + sumOfGoods);
@@ -29,7 +30,7 @@ the total sum of money spent on goods is ${scarf + shoes + coat}
 the approximately sum of money without coins is ${Math.floor(scarf) + Math.floor(shoes) + Math.floor(coat)}
 the sum of money which is rounded number to hundreds is ${Math.floor((Math.floor(scarf) + Math.floor(shoes) + Math.floor(coat))/100)*100}
 is the number is even? - ${(scarf + shoes + coat) % 2 == 0}
-Don\'t forget to take ${500 - (scarf + shoes + coat)} $
+Don\'t forget to take ${customersMoney - (scarf + shoes + coat)} $
 the average amount which is rounded to the second sign after comma is ${((scarf + shoes + coat) / 3).toFixed(2)}
 CONGRATULATIONS ! your discount is ${randomDiscount} %
 you need to pay ${((1 - randomDiscount/100) * (scarf + shoes + coat)).toFixed(2)}
